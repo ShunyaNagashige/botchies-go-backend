5badbde9ee882d1f0f79e095b6c16e94db171500
ShunyaNagashige <nagashige190441091@gmail.com>
2021-06-10 11:17:16 +0900

    fix

--- app/model/peer.go
+++ app/model/peer.go
@@ -88,25 +88,20 @@ func GetWaitingPeers()([]*Peer,error){
 		return nil,err
 	}
 
-
-
-	//nowGMTtime:=utils.JSTtoGMT(time.Now())
-	//今から20分後の時刻
-	//inTwentyMinutes:=time.Now().Add(20*time.Minute)
-
 	//herokuでは，デフォルトのタイムゾーンがPST
 	nowJSTtime:=utils.PSTtoJST(time.Now())
+	
 	//JSTが，現在時刻(JST)から9時間遅れていることを考慮
 	databaseTime:=nowJSTtime.Add(9*time.Hour)
 	inTwentyMinutes:=databaseTime.Add(20*time.Minute)
+
 	//データ読み込み
 	//必ず，CloudFirestoreにて複合インデックスを作成してから，実行すること。
 	//作成していない場合，次のエラーメッセージが表示されると思われる。
 	//その場合，'You can create it here'の後に記述されているurlにアクセスし，指示に従って複合インデックスを作成すること。
 	//2020/09/08 03:46:27 webserver.go:56: action=showTimeline, err= rpc error: code = FailedPrecondition desc = The query requires an index. You can create it here: https://console.firebase.google.com/v1/r/project/{...}
-	//なお，設定時刻が過ぎたPeerは自動的に着信される(はず)ため，time.Now()<=は本当はいらないかも
 	allData:=client.Collection(PeerCollectionName).Where("time_schedule","<=",inTwentyMinutes).Where("time_schedule",">=",databaseTime).Where("isWaiting","==",true).OrderBy("time_schedule",firestore.Asc).Documents(ctx)
-	//allData:=client.Collection(PeerCollectionName).Where("isWaiting","==",true).Documents(ctx)
+	
 	//ドキュメント取得
 	docs,err:=allData.GetAll()
 	if err!=nil{
